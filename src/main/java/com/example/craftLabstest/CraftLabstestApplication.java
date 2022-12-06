78dfae1bf126eebe95140151211d1176611dcb64
Diego Crognale <diego.crognale@mercadolibre.com>
2022-12-06 16:07:06 -0300

    scafolding

---
+++ src/main/java/com/example/craftLabstest/CraftLabstestApplication.java
@@ -0,0 +1,13 @@
+package com.example.craftLabstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CraftLabstestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CraftLabstestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/craftLabstest/CraftLabstestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.craftLabstest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CraftLabstestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
